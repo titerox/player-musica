4204b373960ae959120e9761bf537e8e7a66976d
titerox <tite.182@hotmail.com>
2023-01-30 11:28:26 -0300

    player-completo

--- script/script.js
+++ script/script.js
@@ -1,13 +1,35 @@
 const songName = document.getElementById('song-name');
-const bandName = document.getElementById('band-name')
-const song = document.getElementById('audio')
-const cover = document.getElementById('cover')
-const play = document.getElementById('play')
+const bandName = document.getElementById('band-name');
+const song = document.getElementById('audio');
+const cover = document.getElementById('cover');
+const play = document.getElementById('play');
+const next = document.getElementById('proxima');
+const previous = document.getElementById('voltar');
+const progressBar = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
 
 let isPlaying = false
 
-const 
+const anotherLove = {
+    songName : 'Another Love',
+    artist : 'Tom Odell',
+    file : 'Tom Odell - Another Love'
+}
 
+const takeOnMe = {
+    songName : 'Take on me',
+    artist : 'A-ha',
+    file : 'a-ha - Take on me'
+}
+
+const circles = {
+    songName : 'Circles',
+    artist : 'Post Malone',
+    file : 'Post Malone - Circles'
+}
+
+const playlist = [anotherLove, takeOnMe, circles];
+let index = 0;
 
 
 function playSong(){
@@ -32,4 +54,54 @@ function playPauseDecider(){
     }
 }
 
+
+function initializeSong(){
+    cover.src = `image/${playlist[index].file}.png`;
+    song.src = `musicas/${playlist[index].file}.mp3`;
+    songName.innerText = playlist[index].songName;
+    bandName.innerText = playlist[index].artist;
+}
+
+function previousSong(){
+    if (index === 0){
+        index = playlist.length - 1;
+    } 
+    else {
+    index -= 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+function nextSong(){
+    if (index === playlist.length - 1){
+        index = 0;
+    } 
+    else {
+    index += 1;
+    }
+    initializeSong();
+    playSong();
+}
+
+function updateProgressBar(){
+    song.currentTime
+    song.duration
+    const barWidth = (song.currentTime/song.duration)*100
+    progressBar.style.setProperty('--progress', barWidth+'%')
+}
+
+function jumpTo(event){
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width)*song.duration;
+    song.currentTime = jumpToTime;
+}
+
+initializeSong();
+
 play.addEventListener('click', playPauseDecider);
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
+song.addEventListener('timeupdate', updateProgressBar)
+progressContainer.addEventListener('click', jumpTo);
